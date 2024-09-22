6d30a5aa44acc905ee3874c155baa4654fd24c1f
Pepijn van Loenhout <pepijnvanloenhout@gmail.com>
2024-09-22 22:02:21 +0200

    Fixed render issue due to deletion of views

--- services/userservice/app.js
+++ services/userservice/app.js
@@ -35,7 +35,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.status(500).json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
