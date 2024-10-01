499b126a2a92054cee2fe22d04d1b180fa2563f8
Pepijn van Loenhout <pepijnvanloenhout@gmail.com>
2024-10-01 15:02:32 +0200

    Added some logic

--- gatewayapp/routes/users.js
+++ gatewayapp/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {getUsers} = require('../Controllers/usersController');
+const {getUsers} = require('../controllers/usersController');
 
 /* GET users listing. */
 router.get('/', getUsers);
--- services/userservice/routes/index.js
+++ services/userservice/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const {getUsers} = require('../controllers/usersController');
 
-/* GET home page. */
+/* GET status of service. */
 router.get('/', function(req, res, next) {
   res.send("Welcome to User Service");
 });
+
+/* GET all users. */
+router.get('/All', getUsers);
+
 
 module.exports = router;
