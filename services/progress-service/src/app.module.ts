19dd9a4420d2b2877e121823beba7aed668adae3
Pepijn van Loenhout <pepijnvanloenhout@gmail.com>
2024-10-07 11:42:19 +0200

    added caching mdoule

--- services/progress-service/src/app.module.ts
+++ services/progress-service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
