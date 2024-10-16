169b148806d2a19fb1669e2953af935a1ea76071
Pepijn van Loenhout <pepijnvanloenhout@gmail.com>
2024-10-16 22:44:03 +0200

    removed misplaced folder

--- apps/progress-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ProgressServiceModule } from './progress-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ProgressServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
